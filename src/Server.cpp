9592d9b13d8c7ca301d8036550bd1e3a5812ecde
Sonight5358 <Dvoyaga19@gmail.com>
2026-09-22 23:37:26 +0300

    feat: add message receiving on server

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,6 +2,8 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+#define DEFAULT_BUFLEN 1024
+
 #include <winsock2.h>
 #include <windows.h>
 
@@ -73,13 +75,29 @@ int main()
 	int ClientAddrSize = sizeof(ClientAddr);
 
 	// Accept of incoming connection attempt and saving client socket
-	SOCKET ClientSocket = accept(ServerSocket, (sockaddr*)&ClientAddr, &ClientAddrSize); // accept() will populate second field with client adress
+	SOCKET ClientSocket = accept(ServerSocket, (sockaddr*)&ClientAddr, &ClientAddrSize); // accept() will populate second field with client address
 
 	// If not accepted
 	if (ClientSocket == INVALID_SOCKET)
 	{
 		std::cout << "Not accepted" << '\n';
 		closesocket(ServerSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// Message buffer
+	char buffer[DEFAULT_BUFLEN];
+	int buflen = DEFAULT_BUFLEN;
+
+	// Receive message from client (blocking fuction)
+	Result = recv(ClientSocket, buffer, buflen, 0); // Client socket, buffer, lenght of buffer, advanced receiving option flag
+
+	// If cant recieve
+	if (Result == SOCKET_ERROR)
+	{
+		std::cout << "Not received." << '\n';
+		closesocket(ClientSocket);
 		WSACleanup();
 		return 1;
 	}
